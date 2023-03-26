f59571fa3b0bb28058dd81cedbca1e62337e7c84
UpperFasster <127935644+UpperFasster@users.noreply.github.com>
2023-03-27 02:36:16 +0600

    Add validation

--- src/main/java/com/UpperFasster/Postman/auth/AuthenticationRequest.java
+++ src/main/java/com/UpperFasster/Postman/auth/AuthenticationRequest.java
@@ -1,5 +1,7 @@
 package com.UpperFasster.Postman.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,6 +12,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationRequest {
+    @Email
+    @NotBlank(message = "Email via login can't be null")
     private String email;
+    @NotBlank(message = "Password can't be null")
     private String password;
 }
--- src/main/java/com/UpperFasster/Postman/demo/DemoController.java
+++ src/main/java/com/UpperFasster/Postman/demo/DemoController.java
@@ -12,6 +12,6 @@ public class DemoController {
 
     @GetMapping
     public ResponseEntity<String> sayHello(){
-        return ResponseEntity.ok("Hello from secured endpoint");
+        return ResponseEntity.ok("Check from secured endpoint");
     }
 }
